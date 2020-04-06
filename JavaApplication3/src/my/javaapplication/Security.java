2f23da46bf2afd2e4ee6ffa24ed89357614058c8
Jo Phillips <joxphillips@gmail.com>
2020-04-06 17:45:10 -0600

    Add Functionality to GUI.
    Connected it to the Security file. In a way that I would consider
    temporary but works for demonstration purposes.

--- JavaApplication3/src/my/javaapplication/Security.java
+++ JavaApplication3/src/my/javaapplication/Security.java
@@ -12,10 +12,10 @@ import java.awt.event.ActionListener;
 public class Security 
 {
 private static Timer logoutTimer;
-private int delay = 900000; //15 minutes in miliseconds
-public static  Security single_instance;
+private int delay = 15000; //15 seconds in miliseconds
+public Security single_instance;
 
-private ActionListener logoutListener = new ActionListener()
+public ActionListener logoutListener = new ActionListener()
     {
         @Override
         public void actionPerformed(ActionEvent e)
@@ -24,37 +24,17 @@ private ActionListener logoutListener = new ActionListener()
         }
     };
     
-    private Security()
+    public Security()
     {
+      System.out.println("hi");
         logoutTimer = new Timer(delay, logoutListener);
         logoutTimer.start();
     
     }
     
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, World");
-    }
-    
-    public static void resetTimer()
+    public void resetTimer()
     {
       logoutTimer.restart();
-    }
-    private class LogoutTest
-    {
-     LogoutTest() 
-     {
-        JFrame f = new JFrame();
-        JButton loginButton =new JButton("login");
-        loginButton.setBounds(130,100,100, 40);
-
-        f.add(loginButton);
-        f.setVisible(true);
-     }
-    }
-    { 
-    // static block to initialize instance 
-    single_instance = new Security();
     }
 }
 
