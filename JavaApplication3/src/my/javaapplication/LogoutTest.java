263aab33e3d59b3fc6ee754ea17d8c839838ea03
Jo Phillips <joxphillips@gmail.com>
2020-04-06 18:16:40 -0600

    Add LogoutTest.

---
+++ JavaApplication3/src/my/javaapplication/LogoutTest.java
@@ -0,0 +1,22 @@
+
+/*****************************************************
+ * created by: Jo Phillips
+ * email: joxphillips@gmail.com
+ *  
+ *
+ ****************************************************/
+package my.javaapplication;
+import javax.swing.*;
+
+    public class LogoutTest
+    {
+     LogoutTest() 
+     {
+        JFrame f = new JFrame();
+	f.setBounds(80, 80, 80, 80);
+        JButton loginButton =new JButton("login");
+        loginButton.setBounds(130,100,100, 40);
+        f.add(loginButton);
+        f.setVisible(true);
+     }
+    }
